3b5710bc970a0c3377752ec1add93b6405a589d4
BxptisteM <baptiste.moreau@epitech.eu>
2025-03-20 15:59:33 +0100

    feat(validators): Used FluentValidation package to create valdidators for the login and register requests

---
+++ BuberDinner.Application/Authentication/Commands/Register/RegisterCommandValidator.cs
@@ -0,0 +1,24 @@
+using FluentValidation;
+
+namespace BuberDinner.Application.Authentication.Commands.Register;
+
+public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+{
+    public RegisterCommandValidator()
+    {
+        RuleFor(v => v.FirstName)
+            .MaximumLength(50)
+            .NotEmpty();
+        RuleFor(v => v.LastName)
+            .MaximumLength(50)
+            .NotEmpty();
+        RuleFor(v => v.Email)
+            .MaximumLength(50)
+            .NotEmpty()
+            .EmailAddress();
+        RuleFor(v => v.Password)
+            .MinimumLength(6)
+            .MaximumLength(50)
+            .NotEmpty();
+    }
+}
